cf6be24a2ec1a0fe26c9e12aab819d6942d4c21f
Nguyen Anh Tuan <91422185+HarryNguyen510@users.noreply.github.com>
2022-06-17 15:41:05 +0700

    first commit

---
+++ public/js/config.js
@@ -0,0 +1,49 @@
+const WEB = {
+    title: "Nguyễn Anh Tuấn",
+    favicon: "AT.png",
+};
+
+const USER = {
+    username: "Nguyễn Anh Tuấn",
+    blueTick: true,
+    avatar: "nguyenanhtuan.png",
+    skills: [
+        "Harry",
+        "AT"
+    ],
+    banks: [
+        {
+            name: "Momo",
+            logo: "momo.png",
+            number: "0987020510",
+            owner: "Nguyễn Anh Tuấn",
+            fastTransfer: true
+        },
+        {
+            name: "MB Bank",
+            logo: "mbbank.png",
+            number: "51020019999",
+            owner: "Nguyễn Anh Tuấn"
+        },
+        {
+            name: "BIDV",
+            logo: "bidv.png",
+            number: "46110000562634",
+            owner: "Nguyễn Anh Tuấn"
+        }
+
+        ,{
+            name: "TECHCOMBANK",
+            logo: "techcombank.png",
+            number: "19036687553018",
+            owner: "Nguyễn Anh Tuấn"
+        },
+        {
+            name: "VIETCOMBANK",
+            logo: "vietcombank.png",
+            number: "9987020510",
+            owner: "Nguyễn Anh Tuấn"
+        }
+
+    ]
+};
